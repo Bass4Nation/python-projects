cfda41251ec6f473d4f7c0ee0f8b96247dcc1fff
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-06-25 18:13:13 +0200

    Create restApiToCsharp.py
    Working on it

---
+++ Rest-API-json-to-Csharp/restApiToCsharp.py
@@ -0,0 +1,34 @@
+from types import SimpleNamespace
+import requests
+# https://realpython.com/api-integration-in-python/
+import json
+import pprint
+
+# Test api: https://pokeapi.co 
+api_url = "https://pokeapi.co/api/v2/pokemon/ditto"
+
+def jsonReader(api):
+    response = requests.get(api)
+    jsonData = response.json()
+    jsonDataStr = str(response.json())
+
+    # Pretty print
+    # pprint.pprint(jsonData)
+
+    for key in jsonData.keys():
+        # print(key)
+        print(jsonData[key].keys())
+        # for item in jsonData[key]:
+        #     print(item)
+
+    # print(jsonData['abilities'])
+
+    # parsed = json.loads(jsonData)
+
+    # print(json.dumps(parsed)
+
+
+
+
+
+jsonReader(api_url)
